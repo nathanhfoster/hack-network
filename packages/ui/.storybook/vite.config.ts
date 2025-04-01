a4854c0e30448f12be12bb87fa46d20130637fc4
Nathan <nathan.hugh.foster@gmail.com>
2025-04-01 13:14:31 -0400

    update

--- packages/ui/.storybook/vite.config.ts
+++ packages/ui/.storybook/vite.config.ts
@@ -1,14 +1,22 @@
-import { mergeConfig } from 'vite'
-import { resolve } from 'path'
+import { mergeConfig } from 'vite';
+import { resolve } from 'path';
 
 export default {
   async viteFinal(config) {
     return mergeConfig(config, {
       resolve: {
         alias: {
-          '@': resolve(__dirname, '../src')
-        }
-      }
-    })
-  }
-}
+          '@': resolve(__dirname, '../src'),
+          '@atoms': resolve(__dirname, '../src/lib/atoms'),
+          '@molecules': resolve(__dirname, '../src/lib/molecules'),
+          '@organisms': resolve(__dirname, '../src/lib/organisms'),
+          '@templates': resolve(__dirname, '../src/lib/templates'),
+          '@hack-network/utils': resolve(
+            __dirname,
+            '../../packages/utils/src/index.ts',
+          ),
+        },
+      },
+    });
+  },
+};
