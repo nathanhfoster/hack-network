812aa9c18fc489e0c997ea06cf06139437c689e2
Nathan <nathan.hugh.foster@gmail.com>
2025-04-11 14:49:06 -0400

    V

--- packages/resurrection/vite.config.ts
+++ packages/resurrection/vite.config.ts
@@ -47,14 +47,7 @@ export default defineConfig(() => ({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: [
-        'react',
-        'react-dom',
-        'react/jsx-runtime',
-        'use-context-selector',
-        'immer',
-        'scheduler',
-      ],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         // Enable tree-shaking
         exports: 'named' as const,
